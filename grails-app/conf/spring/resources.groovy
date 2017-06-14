224c11599c9ac36034210d67e0abcac1f6e61d2c
kimgova <kimgova@gmail.com>
2017-06-14 17:05:34 -0600

    Spring security login

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
+import blueloop.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
 }
